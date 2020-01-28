15b9ff77c84d2685f8d15431fa94442e2725d3e3
enmanuelCastillo <enmanuel.castillo@hotmail.com>
2020-01-28 15:03:28 -0400

    inicianizando proyecto en Next.Js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+export default class extends React.Component {
+    render() {
+        return(
+        <>
+         <h1>Hola - NextJs</h1>
+        <p>bienvenido al curso de next JS</p>
+        </>
+        )
+    }
+}
