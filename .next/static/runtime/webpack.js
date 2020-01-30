15b71718668a81314f8c35adec839da368a4c7f9
enmanuelCastillo <enmanuel.castillo@hotmail.com>
2020-01-30 17:14:40 -0400

    Link y recibiendo paramentros

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4bb344d95117f91ebe24";
+/******/ 	var hotCurrentHash = "455057bb915923e6b13f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,74 @@
+import 'isomorphic-fetch'
+import Link from 'next/link'
 export default class extends React.Component {
-    render() {
-        return(
-        <>
-         <h1>Hola - NextJs</h1>
-        <p>bienvenido al curso de next JS</p>
 
-        <img src="/static/perfil.jpg" />
+    static async getInitialProps() {
+      let req = await fetch('https://api.audioboom.com/channels/recommended')
+      let { body: channels } = await req.json()
+
+      return { channels }
+    }
+
+    render() {
+
+        const { channels } = this.props;
+
+        return(
+
+        <div>
+         <header>PodCasts</header>
+            <div className="channels">
+                { channels.map((channel) => (
+                    <Link href={`/channel?id=${channel.id}`}>
+                        <a className="channel">
+                            <img src={channel.urls.logo_image.original} alt="logo del podscasts" />
+                            <h2>{ channel.title }</h2>
+                        </a>
+                    </Link>
+
+                ))}
+            </div>
+
         <style jsx>{`
-         h1 {
-             color: red;
-         }
-         p {
-             color: green;
-         }
-         img {
-             max-with: 50%;
-             display: block;
-             margin: 0 auto;
-         }
+            header {
+                color: #fff;
+                background: #8756ca;
+                padding: 15px;
+                text-align: center;
+            }
+            .channels {
+                display: grid;
+                grid-gap: 15px;
+                padding: 15px;
+                grid-template-columns: repeat(auto-fill, minmax(160px, 1fr))
+            }
+            .channel {
+                display: block;
+                border-radius: 3px;
+                box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
+                margin-bottom: 0.5em;
+            }
+            .channel img {
+                width: 100%;
+            }
+
+            h2 {
+                padding: 5px;
+                font-size: 0.9em;
+                font-weight: 600;
+                margin: 0;
+                text-align: center;
+            }
         `}</style>
-        </>
+
+        <style jsx global>{`
+            body {
+                margin: 0;
+                font-family: system-ui;
+                background: white;
+            }
+            `}</style>
+        </div>
         )
     }
 }
